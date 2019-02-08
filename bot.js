ea2250246e11ed9a52abe69ccc8953e2d4d5c41d
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-02-09 02:30:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['452551643343487007','452551643343487007'];
+const devs = ['307486799645900800','307486799645900800'];
 var prefix = "!";
 const adminprefix = "!"
 const db = require('quick.db');
